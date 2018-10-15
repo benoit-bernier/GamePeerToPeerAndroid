35fdc1463fbce6635a968c6b311c8fd5dff48f39
benoit-bernier <benoit.bernier@etudiant.univ-rennes1.fr>
2018-10-15 11:32:33 +0200

    ajout tuto debut

---
+++ myApp/app/src/main/java/bzh/esir/iot/myapp/DisplayMessageActivity.java
@@ -0,0 +1,13 @@
+package bzh.esir.iot.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message_activity);
+    }
+}
--- myApp/app/src/main/java/bzh/esir/iot/myapp/MainActivity.java
+++ myApp/app/src/main/java/bzh/esir/iot/myapp/MainActivity.java
@@ -1,7 +1,12 @@
 package bzh.esir.iot.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+        public void sendMessage(View view){
+            Intent intent = new Intent(this, DisplayMessageActivity.class);
+            EditText editText = (EditText) findViewById(R.id.editText);
+            String message = editText.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
     }
 }
