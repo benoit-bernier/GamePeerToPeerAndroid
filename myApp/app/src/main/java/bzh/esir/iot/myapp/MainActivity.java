b22c95dd428cd9ac60c56aca9801bbd951a299d3
benoit-bernier <benoit.bernier@etudiant.univ-rennes1.fr>
2018-11-05 22:58:04 +0100

    appplication quizz V0

--- myApp/app/src/main/java/bzh/esir/iot/myapp/MainActivity.java
+++ myApp/app/src/main/java/bzh/esir/iot/myapp/MainActivity.java
@@ -3,25 +3,36 @@ package bzh.esir.iot.myapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    private int score=0;
 
     //@Override
-    public static final String EXTRA_MESSAGE = "bzh.esir.iot.myapp.MESSAGE";
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-        public void sendMessage(View view){
-            Intent intent = new Intent(this, DisplayMessageActivity.class);
-            EditText editText = (EditText) findViewById(R.id.editText);
-            String message = editText.getText().toString();
-            intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);
+    // Plutot choisir de créer une fonction "check reponse", qui prend la BDD et qui incrémente score en cas de bonne réponse.
+
+    public void mauvaiseReponse(View view){
+        //Intent intent = new Intent(this, mauvaiseReponse.class);
+        //startActivity(intent);
+        Button button = findViewById(R.id.button4); //TODO: trouver comment chopper le nom du bouton cliqué
+        button.setText("faux");
+    }
+
+    public void bonneReponse(View view){
+        score++;
+        Intent intent = new Intent(this, bonneReponse.class);
+        startActivity(intent);
+        System.out.println(score);
     }
 }
