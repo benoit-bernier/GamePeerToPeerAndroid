44622cbdf5ee3b337982e311e677c388675ec3e3
blandela <blandel.alex@gmail.com>
2018-12-16 13:25:45 +0100

    Design morse

--- SailorApp/app/src/main/java/alexandre/testapp/RuleMorse.java
+++ SailorApp/app/src/main/java/alexandre/testapp/RuleMorse.java
@@ -14,7 +14,7 @@ public class RuleMorse extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Règle du jeu");
-        builder.setMessage("Tu es en danger ! Envoie un message en morse aux gardes-côtes pour qu'ils viennent t'aider.\n\nAppuie rapidement sur l'écran pour envoyer un signal court / Appuie longtemps sur l'écran pour envoyer un signal long. \n\n Indice : S = . . . / O = _ _ _");
+        builder.setMessage("Tu es en danger ! Envoie un SOS en morse aux gardes-côtes pour qu'ils viennent t'aider.\n\nAppuie rapidement sur l'écran pour envoyer un signal court / Appuie longtemps sur l'écran pour envoyer un signal long. \n\n Indice : S = . . . / O = _ _ _");
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 // rien à faire
