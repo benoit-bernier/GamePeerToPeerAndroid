628f6f0c46842ac5e311a5300a34bba44cf1e5ae
blandela <blandel.alex@gmail.com>
2018-12-17 13:12:33 +0100

    Correction rule drapeau

--- SailorApp/app/src/main/java/alexandre/testapp/RuleDrapeau.java
+++ SailorApp/app/src/main/java/alexandre/testapp/RuleDrapeau.java
@@ -14,7 +14,7 @@ public class RuleDrapeau extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Règle du jeu");
-        builder.setMessage("Moussaillon ! Hisse le drapeau breton pour fêter le retour de nos fidèles compagnon !!\n\nMalheureusement, c'est un un drapeau 'made in China', il te faudra y reprendre à plusieurs fois pour mettre le drapeau à la bonne position.\n\n- Fait un glissement de doigt du haut vers le bas pour abaisser le drapeau.\n\n Fait un glissement de doigt du bas vers le haut pour le monter");
+        builder.setMessage("Moussaillon ! Hisse le drapeau Breton pour fêter le retour de nos fidèles compagnons !!\n\nMalheureusement, c'est un drapeau 'made in China'. Il te faudra t'y reprendre à plusieurs fois pour mettre le drapeau à la bonne position.\n\n- Fais un glissement de doigt du haut vers le bas pour abaisser le drapeau.\n\n- Fais un glissement de doigt du bas vers le haut pour le monter");
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 // rien à faire
